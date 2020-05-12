12e615261380934771b8af6d284e366e13724639
almeidamoises.123@gmail.com <almeidamoises.123@gmail.com>
2020-05-11 22:05:38 -0300

    Implementado GET e POST

--- src/main/java/com/produtos/apirest/repository/ProdutoRepository.java
+++ src/main/java/com/produtos/apirest/repository/ProdutoRepository.java
@@ -4,4 +4,7 @@ import com.produtos.apirest.models.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+
+    Produto findById(long id);
+
 }
